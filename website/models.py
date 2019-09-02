109ba2313651a194fb84d452f62274fbfc026488
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-09-02 10:50:14 -0300

    Changed file upload URL

--- website/models.py
+++ website/models.py
@@ -10,5 +10,5 @@ class Candidate(models.Model):
     github_link = models.URLField()
     eng_level = models.IntegerField()
     salary = models.DecimalField(decimal_places = 2, max_digits = 10)
-    curriculum = models.FileField(null=True, blank=True, upload_to='cv_upload')
+    curriculum = models.FileField(null=True, blank=True, upload_to='cv_upload/')
     
