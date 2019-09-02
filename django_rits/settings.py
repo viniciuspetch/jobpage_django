c0f78b8fe2fb165177a0001149b87efcfdd17325
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-09-02 14:37:30 -0300

    .

--- django_rits/settings.py
+++ django_rits/settings.py
@@ -123,3 +123,18 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/user'
 
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Host for sending e-mail.
+EMAIL_HOST = 'localhost'
+
+# Port for sending e-mail.
+EMAIL_PORT = 1025
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER = ''
+
+EMAIL_HOST_PASSWORD = ''
+
+EMAIL_USE_TLS = False
