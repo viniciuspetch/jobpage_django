8d7e838d2c3b721336ce9eeb4657958f34bc0db5
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-09-02 09:56:28 -0300

    Removed default values from the model

--- website/models.py
+++ website/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Candidate(models.Model):
-    full_name = models.CharField(max_length=123, default="João")
-    email = models.EmailField(default="default@default.com")
-    phone = models.BigIntegerField(default=123456)
-    pres_letter = models.TextField(default="Presentation letter")
-    linkedin_link = models.URLField(default="http://google.com")
-    github_link = models.URLField(default="http://bing.com")
-    eng_level = models.IntegerField(default=1)
-    salary = models.DecimalField(default=123.45, decimal_places = 2, max_digits = 10)
+    full_name = models.CharField(max_length=123)
+    email = models.EmailField()
+    phone = models.BigIntegerField()
+    pres_letter = models.TextField()
+    linkedin_link = models.URLField()
+    github_link = models.URLField()
+    eng_level = models.IntegerField()
+    salary = models.DecimalField(decimal_places = 2, max_digits = 10)
     curriculum = models.FileField(null=True, blank=True, upload_to='cv_upload')
