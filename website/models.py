10abefc1fd0f4a772bb1095b4a26c630f9a839cd
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-09-02 10:15:31 -0300

    Added timestamp

--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 class Candidate(models.Model):
+    datetime = models.DateField(default=timezone.now)
     full_name = models.CharField(max_length=123)
     email = models.EmailField()
     phone = models.BigIntegerField()
@@ -10,3 +12,4 @@ class Candidate(models.Model):
     eng_level = models.IntegerField()
     salary = models.DecimalField(decimal_places = 2, max_digits = 10)
     curriculum = models.FileField(null=True, blank=True, upload_to='cv_upload')
+    
