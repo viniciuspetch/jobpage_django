a16fb6d782d05291a7c711eca7be0f09d237851e
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-09-04 20:03:59 -0300

    Added error messages to forms

--- website/forms.py
+++ website/forms.py
@@ -5,3 +5,12 @@ class CandidateForm(forms.ModelForm):
     class Meta:
         model = Candidate
         fields = '__all__'
+        error_messages = {
+            'full_name': {'required': "Digite o seu nome completo",},
+            'email': {'required': "Digite o seu e-mail",},
+            'phone': {'required': "Digite o seu número de telefone",},
+            'linkedin_link': {'required': "Digite a URL do seu perfil do Linkedin",},
+            'github_link': {'required': "Digite a URL da sua conta do GitHub",},
+            'eng_level': {'required': "Selecione o seu nível de inglês",},
+            'salary': {'required': "Digite a sua pretensão salarial",},
+        }
