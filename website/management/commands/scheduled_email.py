51664e38291fede42179835593a89409c51fee56
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-09-04 23:22:03 -0300

    Removing unused function

--- website/management/commands/scheduled_email.py
+++ website/management/commands/scheduled_email.py
@@ -10,16 +10,3 @@ class Command(BaseCommand):
     
     def handle(self, *args, **kwargs):
         scheduledEmail()
-
-    def handle2(self, *args, **kwargs):
-        today = datetime.today().day
-        print("LOG: "+"Today is day {}".format(today))
-        query = Candidate.objects.filter(datetime__day=today)
-        cdd_qt = len(query)
-        email_msg = "There are {} new candidates today.".format(cdd_qt)
-        print("LOG: "+email_msg)
-        try:
-            email = EmailMessage('Scheduled Message', email_msg, to=['user@gmail.com'])
-            email.send()
-        except:
-            print("ERROR: Couldn't send the e-mail")
